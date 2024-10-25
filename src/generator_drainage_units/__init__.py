0186a1bf73b187b35d1c5173b3127bbd5bee03f5
harm-nomden-sweco <harm.nomden@sweco.nl>
2024-10-25 17:26:48 +0200

    add create vertices

--- src/generator_drainage_units/__init__.py
+++ src/generator_drainage_units/__init__.py
@@ -4,9 +4,17 @@ from .generator_culvert_locations import GeneratorCulvertLocations
 
 def run_generator_culvert_locations(
     path: Path, 
+    distance_vertices: float = 10.0, 
+    max_culvert_length: float = 40.0, 
+    write_results: bool = False
 ):
     culverts_generator = GeneratorCulvertLocations()
 
     # read basis data from folder 0_basisdata
     culverts_generator.read_basisdata_from_case(path=path)
 
+    # generate all vertices every 10 meters
+    culverts_generator.generate_vertices_along_waterlines(
+        distance_vertices=distance_vertices,
+        write_results=write_results
+    )
