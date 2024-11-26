747c161f6afbee1df070c8288fdf5c9de5409157
harm-nomden-sweco <harm.nomden@sweco.nl>
2024-11-27 00:29:37 +0100

    reorganize inits

--- src/generator_drainage_units/generator_culvert_locations/__init__.py
+++ src/generator_drainage_units/generator_culvert_locations/__init__.py
@@ -1,1 +1,71 @@
+import logging
+import time
+from pathlib import Path
+
 from .generator_culverts_locations import GeneratorCulvertLocations
+
+
+def run_generator_culvert_locations(
+    path: Path,
+    distance_vertices: float = 10.0,
+    max_culvert_length: float = 40.0,
+    read_results: bool = False,
+    write_results: bool = False,
+) -> GeneratorCulvertLocations:
+    """Run Generator Culvert Locations (Duikergenerator)
+
+    Parameters
+    ----------
+    path : Path
+        Path to the case directory including directories 0_basisdata and
+        1_tussenresultaat. Directory name is used as name for the case,
+        by default None
+    distance_vertices : float, optional
+        distacne between vertices, by default 10.0
+    max_culvert_length : int, optional
+        maximum culvert length: in case of larger distance between points, connections are not made, by default 40
+    read_results : bool, optional
+        option (True/False) to read previous results from gpkg, by default None
+    write_results : bool, optional
+        option (True/False) to write results to case folder in gpkg, by default None
+
+    Returns
+    -------
+    GeneratorCulvertLocations
+        An instance of the GeneratorCulvertLocations including basisdata and results
+    """
+    start_time = time.time()
+    culverts_generator = GeneratorCulvertLocations(
+        read_results=read_results, write_results=write_results
+    )
+
+    # read basis data from folder 0_basisdata
+    culverts_generator.read_data_from_case(path=path)
+
+    # generate all vertices every 10 meters
+    culverts_generator.generate_vertices_along_waterlines(
+        distance_vertices=distance_vertices, write_results=write_results
+    )
+
+    # generate all potential culverts with max lenght = 40
+    culverts_generator.find_potential_culvert_locations(
+        max_culvert_length=max_culvert_length, write_results=write_results
+    )
+
+    # check intersections culvert with objects and first filter
+    culverts_generator.check_intersections_potential_culverts()
+
+    # assing scores to potential culverts
+    culverts_generator.assign_scores_to_potential_culverts()
+
+    # select best culverts
+    culverts_generator.select_correct_score_based_on_score_and_length()
+
+    # post processing culverts
+    culverts_generator.post_process_potential_culverts()
+
+    # split hydroobjects at endpoints culverts
+    culverts_generator.splits_hydroobjecten_by_endpoints_of_culverts_and_combine()
+
+    logging.info(f"   x Case finished in {round(time.time()-start_time, 3)} seconds")
+    return culverts_generator
--- src/generator_drainage_units/generator_order_levels/__init__.py
+++ src/generator_drainage_units/generator_order_levels/__init__.py
@@ -1,1 +1,29 @@
+from pathlib import Path
+
 from .generator_order_levels import GeneratorOrderLevels
+
+
+def run_generator_order_levels(
+    path: Path,
+    waterschap: str,
+    range_orde_code_min: int,
+    range_orde_code_max: int,
+    read_results: bool = True,
+    write_results: bool = True,
+    create_html_map: bool = False,
+) -> GeneratorOrderLevels:
+    order = GeneratorOrderLevels(
+        waterschap=waterschap,
+        range_orde_code_min=range_orde_code_min,
+        range_orde_code_max=range_orde_code_max,
+        read_results=read_results,
+        write_results=write_results,
+    )
+    order.read_data_from_case(path=path)
+
+    order.find_end_points_hydroobjects()
+    order.generate_rws_code_for_all_outflow_points()
+
+    if create_html_map:
+        order.generate_folium_map()
+    return order
