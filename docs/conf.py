49e0e5a223b2de166d4e99a09fa05ef97e45baef
AnaisWensSweco <anais.wens@sweco.nl>
2025-01-07 22:32:19 +0100

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
-    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
