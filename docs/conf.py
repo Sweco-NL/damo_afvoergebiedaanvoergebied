d14e72ee55cdc561d585aaf7f8732cc6294f08ac
AnaisWensSweco <anais.wens@sweco.nl>
2025-01-07 22:28:59 +0100

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,17 +20,57 @@ import sys
 sys.path.insert(0, os.path.abspath("."))  # Pad naar de code-map relatief aan de source-folder  # Gebruik forward slashes
 
 # Voeg extensies toe
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.autodoc',   # Voor het automatisch genereren van documentatie vanuit docstrings
-    'sphinx.ext.napoleon',   # Voor Google en NumPy stijl docstrings
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "IPython.sphinxext.ipython_console_highlighting",  # lowercase didn't work
+    "sphinx.ext.autosectionlabel",
+    "nbsphinx",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinx_rtd_theme"
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme_options = {
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # Toc options
+    "collapse_navigation": False,
+    "sticky_navigation": False,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# add a logo
+html_logo = "_static/logo_10000_2_2.png"
+
+# Allow errors in notebooks, so we can see the error online
+nbsphinx_allow_errors = True
