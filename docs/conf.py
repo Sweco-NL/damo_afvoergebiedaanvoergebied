d71c511b608501d84725a2c7709bf4eb1c2a0927
AnaisWensSweco <anais.wens@sweco.nl>
2025-01-08 13:10:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -62,13 +62,6 @@ html_theme_options = {
     "titles_only": False,
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-# add a logo
-html_logo = "_static/logo_10000_2_2.png"
 
 # Allow errors in notebooks, so we can see the error online
 nbsphinx_allow_errors = True
