a9853518512721855a73a6490914a9ce82c20ac4
harm-nomden-sweco <harm.nomden@sweco.nl>
2025-01-09 16:25:54 +0100

    update

--- src/generator_drainage_units/__init__.py
+++ src/generator_drainage_units/__init__.py
@@ -1,6 +1,5 @@
 from .generator_network_lumping import (
     run_generator_network_lumping,
-    run_network_lumping_with_random_selection_splits,
     GeneratorNetworkLumping,
 )
 from .generator_order_levels import run_generator_order_levels, GeneratorOrderLevels
