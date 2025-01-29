260d697665af337e0b3d53e027a0018ae6133c80
harm-nomden-sweco <harm.nomden@sweco.nl>
2025-01-29 08:19:32 +0100

    carrousel

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
+    "sphinx_carousel.carousel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
